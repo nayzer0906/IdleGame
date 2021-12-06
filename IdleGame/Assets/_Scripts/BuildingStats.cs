d4f872d0d385d479af6cbf916492a689690a9280
nayzer0906 <72375303+nayzer0906@users.noreply.github.com>
2021-12-06 15:03:01 +0600

    Popup appearing and disappearing

---
+++ IdleGame/Assets/_Scripts/BuildingStats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingStats : MonoBehaviour
+{
+    [SerializeField] private TMP_Text currentLevel;
+    [SerializeField] private TMP_Text nextLevel;
+    [SerializeField] private Slider progressBar;
+    [SerializeField] private TMP_Text upgradePrice;
+    private Building building;
+}
--- IdleGame/Assets/_Scripts/GameManager.cs
+++ IdleGame/Assets/_Scripts/GameManager.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform popup;
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (Input.touchSupported)
+        {
+            var touches = Input.touchCount;
+            if (touches > 0)
+            {
+                for (int i = 0; i < touches; i++)
+                {
+                    var touch = Input.GetTouch(i);
+                    if (touch.phase == TouchPhase.Began)
+                    {
+                        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                        RaycastHit hit;
+                        if (Physics.Raycast(ray, out hit) && !EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+                        {
+                            if (hit.transform.GetComponent<Building>())
+                            {
+                                popup.gameObject.SetActive(true);
+                            }
+                            else
+                            {
+                                popup.gameObject.SetActive(false);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit) && !EventSystem.current.IsPointerOverGameObject())
+                {
+                    if (hit.transform.GetComponent<Building>())
+                    {
+                        popup.gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        popup.gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
     }
 }
